2c0e83452e4bb23f42f7c88c10e18c0f3b79f838
Aobakwe <bakkies2116@gmail.com>
2024-02-29 05:17:12 +0200

    Implement template caching

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.core.cache import cache
 from .models import Post
 
 def blog_list(request):
-    posts = Post.objects.all()
+    # Check if blog posts are cached
+    posts = cache.get('blog_posts')
+
+    if not posts:
+        # Fetch blog posts from database
+        posts = Post.objects.all()
+        # Cache blog posts for 5 minutes
+        cache.set('blog_posts', posts, 300)
+
     return render(request, 'blog.html', {'posts': posts})
 
 def post_detail(request, pk):
